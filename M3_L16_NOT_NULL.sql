37d89be03e05d1ad53e9f7149a2e98c2ba625750
karinakupniewska <75994831+karinakupniewska@users.noreply.github.com>
2021-01-09 10:06:42 +0100

    Delete M3_L16_NOT_NULL.sql

--- M3_L16_NOT_NULL.sql
+++
@@ -1,9 +0,0 @@
---ograniczenie jest widoczne w zakładce columns jako check box, nie widać ograniczenia not null w zakładce constraints
-CREATE TABLE sales (
-	id integer NOT NULL,
-	sales NUMERIC CHECK (sales > 1000)
-);
---usuwanie ogranicznia z powyższej tabeli
-ALTER TABLE sales ALTER COLUMN id DROP NOT NULL;
-
-
